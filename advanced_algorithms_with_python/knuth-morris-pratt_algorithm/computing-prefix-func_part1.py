1789c61f348f8bbc9f087370ad701715e8dee354
Dmitry Jum <xfiler@ymail.com>
2023-01-08 22:50:23 -0800

    completes computing th eprefix function part 1 exercise

---
+++ advanced_algorithms_with_python/knuth-morris-pratt_algorithm/computing-prefix-func_part1.py
@@ -0,0 +1,12 @@
+def prefix_function(pattern):
+  pi = [0 for i in range(len(pattern))]
+  for i in range(1, len(pattern)):
+    if (pattern[:pi[i - 1] + 1] == pattern[i - pi[i - 1]:i + 1]):
+      pi[i] = pi[i - 1] + 1
+    elif (pattern[:pi[i - 1]] == pattern[i - pi[i - 1] + 1:i + 1]):
+      pi[i] = pi[i - 1]
+    else:
+      for j in range(pi[i - 1], -1, -1):
+        if (pattern[:j] == pattern[i - j + 1: i + 1]):
+          pi[i] = j
+          break
