b539b5d15ccc7ceaf209d47aee15833576958c6c
Dmitry Jum <xfiler@ymail.com>
2023-04-04 14:36:10 -0700

    adds typeguards lesson

---
+++ typescript/typeguards/index.ts
@@ -0,0 +1,7 @@
+function formatStatistic(stat: string | number) {
+  if (typeof stat === 'number') return stat.toFixed(2);
+  if (typeof stat === 'string') return stat.toUpperCase();
+}
+
+console.log(formatStatistic('Win'));
+console.log(formatStatistic(0.364));
